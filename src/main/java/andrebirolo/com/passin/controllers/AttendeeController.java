df99fc9dfa35a2bcd5b8fd340b1b4abfe65e36ee
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2024-04-06 12:21:19 -0300

    Criação dos endpoints de check-in e crachá

--- src/main/java/andrebirolo/com/passin/controllers/AttendeeController.java
+++ src/main/java/andrebirolo/com/passin/controllers/AttendeeController.java
@@ -1,10 +1,26 @@
 package andrebirolo.com.passin.controllers;
 
+import andrebirolo.com.passin.dto.attendee.AttendeeBadgeResponseDTO;
+import andrebirolo.com.passin.services.AttendeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/attendees")
+@RequiredArgsConstructor
 public class AttendeeController {
+
+    private final AttendeeService attendeeService;
+
+    @GetMapping("attendees/{attendeeId}/badge")
+    public ResponseEntity<AttendeeBadgeResponseDTO> getAttendeeBadge(@PathVariable String attendeeId, UriComponentsBuilder uriBuilder) {
+        AttendeeBadgeResponseDTO response = this.attendeeService.getAttendeeBadge(attendeeId, uriBuilder);
+        return ResponseEntity.ok(response);
+    }
 
 }
--- src/main/java/andrebirolo/com/passin/controllers/EventController.java
+++ src/main/java/andrebirolo/com/passin/controllers/EventController.java
@@ -1,6 +1,8 @@
 package andrebirolo.com.passin.controllers;
 
+import andrebirolo.com.passin.dto.attendee.AttendeeIdDTO;
 import andrebirolo.com.passin.dto.attendee.AttendeeListResponseDTO;
+import andrebirolo.com.passin.dto.attendee.AttendeeRequestDTO;
 import andrebirolo.com.passin.dto.event.EventIdDTO;
 import andrebirolo.com.passin.dto.event.EventRequestDTO;
 import andrebirolo.com.passin.dto.event.EventResponseDTO;
@@ -15,20 +17,28 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RequestMapping("/events")
 @RequiredArgsConstructor
 public class EventController {
-    private final EventService service;
+    private final EventService eventService;
     private final AttendeeService attendeeService;
 
     @GetMapping("/{eventId}")
     public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String eventId) {
-        EventResponseDTO event = this.service.getEventDetail(eventId);
+        EventResponseDTO event = this.eventService.getEventDetail(eventId);
         return ResponseEntity.ok(event);
     }
 
     @PostMapping
     public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriBuilder) {
-        EventIdDTO event = this.service.createEvent(body);
+        EventIdDTO event = this.eventService.createEvent(body);
         var uri = uriBuilder.path("/events/{id}").buildAndExpand(event.id()).toUri();
         return ResponseEntity.created(uri).body(event);
+    }
+
+    @PostMapping("/{eventId}/attendees")
+    public ResponseEntity<AttendeeIdDTO> registerParticipant(@PathVariable String eventId, @RequestBody AttendeeRequestDTO body, UriComponentsBuilder uriBuilder) {
+        AttendeeIdDTO attendeeIdDTO = this.eventService.registerAttendeeOnEvent(eventId, body);
+        var uri = uriBuilder.path("/attendees/{attendeId}/badge").buildAndExpand(attendeeIdDTO.attendeeId()).toUri();
+
+        return ResponseEntity.created(uri).body(attendeeIdDTO);
     }
 
     @GetMapping("/events/attendees/{eventId}")
